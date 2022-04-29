d7b209ae62d32d4e53a6d338c95eb6a5117d7fd9
nitishgupta978 <nitishgupta9785@gmail.com>
2022-04-29 09:33:35 +0530

    Welcome to Employee_Payroll

---
+++ src/com/bridgelabz/employee/payroll/Welcome_Emp_Payroll.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employee.payroll;
+
+public class Welcome_Emp_Payroll {
+	public static void main(String[] args) {
+		System.out.println("Welcome in Employee_PayRoll");
+	}
+}
